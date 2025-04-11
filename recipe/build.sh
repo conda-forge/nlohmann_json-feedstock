a42d28203e31660c34a5b17f8532b14e1dbdd370
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-04-11 18:49:28 +0200

    update files

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cmake \
+    -G Ninja \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     $SRC_DIR \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DBUILD_TESTING=OFF \
     -DJSON_MultipleHeaders=ON \
     ${CMAKE_ARGS}
-make install
+
+ninja install
